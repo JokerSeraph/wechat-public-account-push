9900bc5cde53d26746b751cd82b26811f30407b1
JokerSeraph <92203774+JokerSeraph@users.noreply.github.com>
2024-04-11 15:17:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'opx1s5s1a6YMrIi3UuaQALpd7d_4',
+      id: 'opx1s5pVQuOmNNZKehWpvspPiivU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'OpRoyLJWjnQm6586qL-L_MIbKiG865gg-H83VgzmNes',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -29,11 +29,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1997', date: '10-23',
+          type: '*生日', name: '宝贝', year: '1997', date: '09-22',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1999', date: '01-03',
+          type: '生日', name: '穆苗', year: '1997', date: '10-23',
         },
         {
           type: '节日', name: '相识纪念日', year: '2024', date: '02-14',
